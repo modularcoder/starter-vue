fc6ba7e785625c8cbafc31fc725a735fa7e04766
Gevorg Harutyunyan <gevharut@gmail.com>
2021-04-16 01:26:01 +0400

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue'
+import App from './App.vue'
+import store from './_store'
+import router from './router.js'
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(store).use(router).mount('#app')
